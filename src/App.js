f57502507cd8980efca28a4985d86b69c614cc9e
kanemobile <makane9305@gmail.com>
2018-10-06 00:08:42 +0000

    Création d'un component TableRow

--- src/App.js
+++ src/App.js
@@ -1,27 +1,80 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      data: [
+        {
+          "id": 1,
+          "name": "Foo",
+          "age": "20"
+        },
+        {
+          "id": 2,
+          "name": "Bar",
+          "age": "30"
+        },
+        {
+          "id": 3,
+          "name": "Baz",
+          "age": "40"
+        }
+      ]
+    }
+  }
+
   render() {
+    
+    var myStyle = {
+      fontSize: 20,
+      color: '#000852'
+    };
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div style={myStyle}>
+        <Header />
+        <table>
+          <tbody>
+            {this.state.data.map((person, i) => <TableRow key={i} data={person}/>)}
+          </tbody>
+        </table>
       </div>
     );
+  }
+}
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Header</h1>
+      </div>
+    )
+  }
+}
+/*
+class Content extends Component {
+  render() {
+    return(
+      <div>
+        <h2>Content</h2>
+        <p>The content text!!!</p>
+      </div>
+    )
+  }
+}
+*/
+class TableRow extends Component {
+  render() {
+    return (
+      <tr>
+        <td>{this.props.data.id}</td>
+        <td>{this.props.data.name}</td>
+        <td>{this.props.data.age}</td>
+      </tr>
+    )
   }
 }
 
